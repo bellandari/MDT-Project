00a7c63bba77b28e5fbc4b480a41005fedf94fda
bellandari <70709767+bellandari@users.noreply.github.com>
2023-07-11 20:05:45 -0600

    Working Update

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note, Ped
 from . import db
 import json
 
@@ -14,9 +14,17 @@ def home():
 
 @views.route('/search', methods=['GET', 'POST'])
 @login_required
-def search():
+def search():     
+    
+    firstName = request.form.get('firstName')
+    lastName = request.form.get('lastName')
+    dob = request.form.get('dob')
+    
+    results = Ped.query.filter(firstName==firstName).filter(lastName==lastName).filter(dob==dob).limit(1).first()
+    
+    print(results)
         
-    return render_template("search.html", user=current_user)
+    return render_template("search.html", user=current_user, results = results)
 
 @views.route('/help', methods=['GET', 'POST'])
 @login_required
